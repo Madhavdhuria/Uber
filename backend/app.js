8055975e6227d172fd614fee561adf8b9472d423
Madhavdhuria <madhavdhuria556@gmail.com>
2025-06-01 15:15:00 +0530

    cors solved

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,11 @@ connectToDb();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
+    origin: "*",
+    credentials: false,
   })
 );
 
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-
- async function connectToDb() {
-    try {
-        await mongoose.connect("mongodb+srv://madhav:2003%40Mongo2024@cluster0.xaijqpg.mongodb.net/uber");
-        console.log("Connected to DB!");
-    } catch (error) {
-        console.error("Failed to connect to DB:", error);
-    }
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.DBURL);
+    console.log("Connected to DB!");
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+  }
 }
 
-module.exports=connectToDb;
+module.exports = connectToDb;
