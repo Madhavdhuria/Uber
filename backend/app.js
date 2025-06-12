9f8e3f8ed92a930b60e2e9b47fe9dc35fda257c1
Madhavdhuria <madhavdhuria556@gmail.com>
2025-06-12 10:32:14 +0530

    fixing cors issue

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "https://uber-frontend-wheat.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://uber-beta-two.vercel.app",
     credentials: true,
   })
 );
